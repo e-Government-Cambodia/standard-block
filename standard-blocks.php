912e8a6326177b8b01d17fdff5aa028931f7b0ee
Chetra Chann <channchetra@gmail.com>
2023-08-18 15:58:50 +0700

    Added update checker.

--- standard-blocks.php
+++ standard-blocks.php
@@ -27,3 +27,19 @@ function kmstdblocks_standard_blocks_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'kmstdblocks_standard_blocks_block_init' );
+
+require 'plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/e-Government-Cambodia/standard-blocks/',
+	__FILE__,
+	'standard-blocks'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
